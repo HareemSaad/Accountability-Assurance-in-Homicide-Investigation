1ef6c3e83c08ddc3ce1c6b95165fab5c51963f25
HareemSaad <hareems20014@outlook.com>
2023-09-16 19:24:54 +0500

    create and update functions

--- back-end/contracts/src/Access.sol
+++ back-end/contracts/src/Access.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Error.sol";
 
 contract Access is AccessControl {
 
--- back-end/contracts/src/Cases.sol
+++ back-end/contracts/src/Cases.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Cases {
+import "./Access.sol";
+import "./Officers.sol";
 
+contract Cases is Access {
+
+    using Strings for string;
+
+    Officers officersContract;
+
+    event NewCase(uint caseId, address indexed initiator);
+    event CaseStatusUpdated(uint caseId, address indexed initiator, CaseStatus oldStatus, CaseStatus newStatus);
+    event AddOfficer(uint caseId, address indexed initiator, address indexed officer);
+    
     enum CaseStatus {
         OPEN, CLOSED, COLD
     }
@@ -31,26 +42,52 @@ contract Cases {
 
     struct Case {
         CaseStatus status;
+        address[] officers;
         Figure[] figures;
         Evidence[] evidences;   
     }
 
+    constructor(address _officersContract) {
+        officersContract = Officers(_officersContract);
+    }
+
     mapping (uint => Case) _case;
+
+    function addCase(uint _caseId, Figure[] calldata _figures) external onlyRole(CAPTAIN_ROLE) {
+
+        Case storage newCase = _case[_caseId];
+        newCase.status = CaseStatus.OPEN;
+        newCase.figures = _figures;
+
+        emit NewCase(_caseId, msg.sender);
+    }
+
+    function updateCaseStatus(uint _caseId, CaseStatus _status) external onlyRole(CAPTAIN_ROLE) {
+
+        CaseStatus oldStatus = _case[_caseId].status;
+        _case[_caseId].status = _status;
+
+        emit CaseStatusUpdated(_caseId, msg.sender, oldStatus, _status);
+    }
+
+    function addOfficerInCase(uint _caseId, address _officer) external onlyRole(CAPTAIN_ROLE) {
+
+        if(officersContract.isValidOfficer(_officer)) { revert InvalidOfficer(); }
+
+        _case[_caseId].officers.push(_officer); 
+
+        emit AddOfficer(_caseId, msg.sender, _officer);
+    }
+
+    // function addFigure(uint _caseId, Figure _figure) external onlyRole(CAPTAIN_ROLE) {
+
+    //     _case[_caseId].figures.push(_figure);
+
+    //     emit NewFigureInCase(_caseId, msg.sender, _figure.);
+    // }
 
 }
 
-/** I can ...
- * add a new case
- * close an opened case
- * periodically call the contract to dormant inactive cases
- * add suspects in a case
- * add evidence in a case
- * add witnesses in a case
- * add alibis in a case...?
- * add victim in a case
- * add murder weapon in a case
- * add culprit in a case
- */
 
 /** Only the ... 
  * captain can open a case
