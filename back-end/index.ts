9e51bb32e2b524b4c539730e032b8092c8658855
HareemSaad <hareems20014@outlook.com>
2023-09-18 01:44:49 +0500

    event listeners

--- back-end/index.ts
+++ back-end/index.ts
@@ -5,6 +5,9 @@ import path from 'path';
 import { Console } from 'console';
 import { validateOfficer } from "./scripts/OfficerInfo";
 import dotenv from 'dotenv';
+import { ethers } from 'ethers';
+const officerABI = require("./contracts/out/Officers.sol/Officers.json");
+const caseABI = require("./contracts/out/Cases.sol/Cases.json");
 dotenv.config()
 let poolRoutes = require('./routes/routes');
 const port = 3000;
@@ -14,17 +17,94 @@ app.use(express.json())
 app.use(cors());
 app.use(bodyParser.json());
 
-const rhsUrl = process.env.RHS_URL as string;
-const rpcUrl = process.env.RPC_URL as string;
-const contractAddress = process.env.CONTRACT_ADDRESS as string;
-const walletKey = process.env.WALLET_KEY as string;
-const circuitsFolder = process.env.CIRCUITS_PATH as string;
+
+const officerContractAddress = process.env.OFFICER_CONTRACT_ADDRESS;
+const caseContractAddress = process.env.CASE_CONTRACT_ADDRESS;
+const privateKey = process.env.WALLET_KEY;
+const rpc = process.env.RPC_URL;
+const provider = ethers.getDefaultProvider(11155111, {
+  alchemy: rpc
+});
+const signer = new ethers.Wallet(privateKey as string, provider);
+const contract = new ethers.Contract(officerContractAddress as string, officerABI.abi, signer);
+const casecontract = new ethers.Contract(caseContractAddress as string, caseABI.abi, signer);
+
+const wsprovider = new ethers.WebSocketProvider(process.env.WS_RPC_URL as string)
+const wssigner = new ethers.Wallet(privateKey as string, wsprovider);
+const wscontract = new ethers.Contract(officerContractAddress as string, officerABI.abi, wssigner);
+const wscasecontract = new ethers.Contract(caseContractAddress as string, caseABI.abi, wssigner);
 
 main().catch(err => console.log(err));
 
 async function main() {
 
-  validateOfficer('0x86D5cA9d24ecE1d8c35a45b83Ba15B1B9e11BD50');
+  // await validateOfficer('0x86D5cA9d24ecE1d8c35a45b83Ba15B1B9e11BD50');
+
+  try {
+    wscontract.on(wscontract.filters.NewOfficer, (officer, newRank, when, from, event) => {
+      console.log('NewOfficer event detected:');
+      console.log('Officer:', officer);
+      console.log('New Rank:', newRank.toString());
+      console.log('When:', when.toString());
+      console.log('From:', from);
+    });
+
+    wscasecontract.on(wscasecontract.filters.NewCase, (caseId, initiator, event) => {
+      console.log('NewCase event detected:');
+      console.log('Case ID:', caseId.toString());
+      console.log('Initiator:', initiator);
+    });
+
+    wscasecontract.on(wscasecontract.filters.CaseStatusUpdated, (caseId, initiator, oldStatus, newStatus, event) => {
+      console.log('CaseStatusUpdated event detected:');
+      console.log('Case ID:', caseId.toString());
+      console.log('Initiator:', initiator);
+      console.log('Old Status:', oldStatus.toString());
+      console.log('New Status:', newStatus.toString());
+    });
+
+    wscasecontract.on(wscasecontract.filters.AddOfficer, (caseId, initiator, officer, event) => {
+      console.log('AddOfficer event detected:');
+      console.log('Case ID:', caseId.toString());
+      console.log('Initiator:', initiator);
+      console.log('Officer:', officer);
+    });
+
+    wscasecontract.on(wscasecontract.filters.NewParticipantInCase, (caseId, initiator, suspectId, category, dataHash, data, event) => {
+      console.log('NewParticipantInCase event detected:');
+      console.log('Case ID:', caseId.toString());
+      console.log('Initiator:', initiator);
+      console.log('Suspect ID:', suspectId.toString());
+      console.log('Category:', category.toString());
+      console.log('Data Hash:', dataHash);
+      console.log('Data:', data);
+    });
+
+    wscasecontract.on(wscasecontract.filters.NewEvidenceInCase, (caseId, initiator, evidenceId, category, dataHash, data, event) => {
+      console.log('NewEvidenceInCase event detected:');
+      console.log('Case ID:', caseId.toString());
+      console.log('Initiator:', initiator);
+      console.log('Evidence ID:', evidenceId.toString());
+      console.log('Category:', category.toString());
+      console.log('Data Hash:', dataHash);
+      console.log('Data:', data);
+    });
+  } catch (error) {
+    console.error('Error subscribing to the NewOfficer event:', error);
+  } 
+
+  //onboard(address _officer, string memory name, string memory badge, Rank rank)
+  // await contract.onboard(
+  //   '0x8233c83CbF40Aa2F6407DdDb9f91C6d126CF31a9',
+  //   "Bob",
+  //   "DET_100",
+  //   1
+  // );
+
+  // console.log(await contract.CAPTAIN_ROLE(), signer.address)
+
+  // console.log(await contract.grantRole(await contract.CAPTAIN_ROLE(), signer.address))
+  await casecontract.addCase(129);
 
 }
 
