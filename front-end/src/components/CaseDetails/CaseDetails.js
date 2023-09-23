3d1232acee101583247adf277112a37c52693ce7
HareemSaad <hareems20014@outlook.com>
2023-09-23 17:23:30 +0500

    case detail page fix

--- front-end/src/components/CaseDetails/CaseDetails.js
+++ front-end/src/components/CaseDetails/CaseDetails.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import "./CaseDetails.css"
 
 const CaseDetailsPage = () => {
   const { caseId } = useParams();
+  let navigate = useNavigate();
+
+  const goto = (e) => {
+    const { name } = e.target;
+    console.log("params ::", name, caseId)
+    navigate(`/${name}/${caseId}`);
+  }
+
   return (
     <div className=''>
       <div className='m-4 d-flex flex-row justify-content-between'>
         <h2>Case Number: {caseId}</h2>
-        <DropdownButton id="edit-case-detail" title="Edit Case Detail" className='me-5'>
-          <Dropdown.Item href={`/add-evidence/${caseId}`}>Add Evidence</Dropdown.Item>
-          <Dropdown.Item href={`/add-participant/${caseId}`}>Add Participant</Dropdown.Item>
-        </DropdownButton>
       </div>
-
-      <Form className='m-5'>
-        <Form.Group className="mb-4" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Details</Form.Label>
-          <Form.Control as="textarea" rows={3} className='ms-2' />
-        </Form.Group>
-      </Form>
-
-      <div className="d-flex justify-content-end me-5 mb-5">
-        <button className='btn btn-primary me-md-3 w-25' type='button'>Save</button>
+      <div className="row-flex">
+        <button className='case-nav-btn' name="add-evidence" onClick={(e) => goto(e)}>Add Evidence</button>
+        <button className='case-nav-btn' name="add-participant" onClick={(e) => goto(e)}>Add Participant</button>
       </div>
     </div>
   );
