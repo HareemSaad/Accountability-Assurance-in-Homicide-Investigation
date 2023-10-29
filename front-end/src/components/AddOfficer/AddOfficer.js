aeffa8b73583f093920fd4e87fde3456ed8d0422
HareemSaad <hareems20014@outlook.com>
2023-10-29 21:21:18 +0500

    add officer works

--- front-end/src/components/AddOfficer/AddOfficer.js
+++ front-end/src/components/AddOfficer/AddOfficer.js
@@ -5,6 +5,8 @@ import { notify } from "../utils/error-box/notify";
 import "react-toastify/dist/ReactToastify.css";
 import './AddOfficer.css';
 import Dropdown from 'react-bootstrap/Dropdown';
+import OfficersABI from "./../OfficersABI.json";
+import { readContract, signMessage, waitForTransaction, writeContract } from '@wagmi/core'
 
 export const AddOfficer = () => {
 
@@ -15,13 +17,18 @@ export const AddOfficer = () => {
     const [officerInfo, setOfficerInfo] = useState({
         address: '',
         name: '',
-        status: '',
         rank: '',
-        batch: '',
+        badge: '',
     });
     const rankArray = ['Null', 'Officer', 'Detective', 'Captain'];
     const statusArray = ['Inactive', 'Active', 'Retired', 'Fired'];
     const officerContractAddress = process.env.REACT_APP_OFFICER_CONTRACT;
+    const RoleBytes = {
+        3: "0xd1caa20fe64a17576895d331b6b815baf91df37730e70e788978bc77ac7559b4",
+        2: "0x9bcceb74634ac977676ecaf8900febd8cc8358719b06c206b86e9e10f6758bc2",
+        1: "0xbbecb2568601cb27e6ced525237c463da94c4fb7a9b98ac79fd30fd56d8e1b53",
+        0: "",
+    }
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -30,26 +37,40 @@ export const AddOfficer = () => {
         console.log("value :: ", value)
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
         if (officerInfo.name === '') {
             notify("error", `Officer Name is empty`);
-        } else {
-            console.log("Officer Name :: ", officerInfo.name)
+        } else if (officerInfo.address === '') {
+            notify("error", `Officer Address is empty`);
+        } else if (officerInfo.rank === '' || officerInfo.rank === 0) {
+            notify("error", `Select Officer Rank`);
+        } else if (officerInfo.badge === '') {
+            notify("error", `Badge Number is empty`);
         }
-    }
+        console.log(officerInfo)
+        // call contract
+        const { hash } = await writeContract({
+            address: officerContractAddress,
+            abi: OfficersABI.abi,
+            functionName: 'onboard',
+            args: [officerInfo.address, officerInfo.name, officerInfo.badge, RoleBytes[officerInfo.rank]],
+            chainId: 11155111
+        })
+        console.log("hash :: ", hash)
 
-    // Function to handle dropdown item selection
-    const handleStatusDropdownSelect = (categoryValue) => {
-        setSelectedStatusValue(categoryValue);
-        const name = 'category';
-        setOfficerInfo({ ...officerInfo, [name]: categoryValue });
-    };
+        // wait for txn
+        const result = await waitForTransaction({
+            hash: hash,
+        })
+        console.log("Transaction result:", result);
+    }
 
     // Function to handle dropdown item selection
     const handleRankDropdownSelect = (categoryValue) => {
         setSelectedRankValue(categoryValue);
-        const name = 'category';
+        const name = 'rank';
         setOfficerInfo({ ...officerInfo, [name]: categoryValue });
     };
 
@@ -83,26 +104,7 @@ export const AddOfficer = () => {
                         <label htmlFor="officerRank" className="col-form-label"><b><em>Officer Batch:</em></b></label>
                     </div>
                     <div className="col-9 input">
-                        <input type="text" name='batch' id="officerBatch" placeholder='Enter officer Batch Here' className="form-control" onChange={handleChange}></input>
-                    </div>
-                </div>
-
-                {/* Officer Status dropdown */}
-                <div className="row g-3 align-items-center m-3">
-                    <div className="col-2">
-                        <label htmlFor="officerStatus" className="col-form-label"><b><em>Officer Status:</em></b></label>
-                    </div>
-
-                    <div className="col-9">
-                        <Dropdown>
-                            <Dropdown.Toggle variant="secondary" id="officerStatus" className='dropdown'> {selectedStatusValue ? statusArray[selectedStatusValue] : 'Select a Status'} </Dropdown.Toggle>
-
-                            <Dropdown.Menu className='dropdown'>
-                                {statusArray.map((category, index) => (
-                                    <Dropdown.Item name='status' key={index} onClick={() => handleStatusDropdownSelect(index)}> {category} </Dropdown.Item>
-                                ))}
-                            </Dropdown.Menu>
-                        </Dropdown>
+                        <input type="text" name='badge' id="officerBatch" placeholder='Enter officer Batch Here' className="form-control" onChange={handleChange}></input>
                     </div>
                 </div>
 
@@ -126,7 +128,7 @@ export const AddOfficer = () => {
                 </div>
 
 
-                <button className='btn btn-primary d-grid gap-2 col-4 mx-auto m-5 p-2' type="submit" onClick={(e) => handleSubmit(e)}>
+                <button className='btn btn-primary d-grid gap-2 col-4 mx-auto m-5 p-2' type="submit" onClick={async (e) => await handleSubmit(e)}>
                     Add Officer
                 </button>
 
