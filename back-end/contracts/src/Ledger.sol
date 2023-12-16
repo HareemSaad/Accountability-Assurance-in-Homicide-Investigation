4fb4a7d4b542337220ede3e2cb9a3d1bd113dc71
HareemSaad <hareems20014@outlook.com>
2023-12-17 02:44:37 +0500

    signature for add branch

--- back-end/contracts/src/Ledger.sol
+++ back-end/contracts/src/Ledger.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "./Utils/Error.sol";
+import "./Libraries/CreateBranch.sol";
 
-contract Ledger {
+contract Ledger is EIP712 {
 
     using Strings for string;
 
     event BranchUpdate(
-
+        bytes32 indexed id,
+        string precinctAddress,
+        uint indexed jurisdictionArea
     );
 
-    constructor(address _officer, string memory _name, bytes32 _badge, bytes32 _branchId) {
-        // _onboard(_officer, _name, _badge, _branchId, Rank.MODERATOR);
+    constructor(address _officer, string memory _name, bytes32 _badge, bytes32 _branchId) EIP712("Ledger", "1") {
     }
 
     struct Officer {
@@ -44,24 +48,70 @@ contract Ledger {
         _;
     }
 
+    modifier onlyModerator(uint _code) {
+        if (!moderators[msg.sender][_code]) { revert OnlyModerator(); }
+        _;
+    }
+
     mapping (address => Officer) public officers;
+    mapping (address => mapping (uint => bool)) public moderators;
+    mapping (uint => uint) public moderatorCount;
     mapping (bytes32 => Branch) public branches;
     
     // add a new branch
     function createBranch(
         string memory _id, 
         string memory _precinctAddress,
-        uint _jurisdictionArea
-    ) external onlyRank(Rank.MODERATOR) {
-        if(_id.equal("") || _precinctAddress.equal("") || _jurisdictionArea == 0) revert InvalidId();
+        uint _jurisdictionArea,
+        bytes[] memory _signatures,
+        address[] memory _signers
+    ) external onlyModerator(_jurisdictionArea) {
+        if(_id.equal("") || _precinctAddress.equal("") || _jurisdictionArea == 0) revert InvalidInput();
 
-        Branch storage _branch = branches[keccak256(abi.encode(_id))];
+        bytes32 id = keccak256(abi.encode(_id));
+        Branch storage _branch = branches[id];
 
         if(!_branch.precinctAddress.equal("")) revert BranchAlreadyExists();
+
+        if(_signatures.length != _signers.length) revert LengthMismatch();
+        if((moderatorCount[_jurisdictionArea] / 2) + 1  > _signers.length) revert NotEnoughSignatures();
+
+
+        bytes32 messageHash = CreateBranch.hash(CreateBranch.CreateBranchVote(
+            _precinctAddress,
+            _jurisdictionArea,
+            id
+        ));
+
+        _validateSignatures(messageHash, _signatures, _signers, _jurisdictionArea);
 
         _branch.precinctAddress = _precinctAddress;
         _branch.jurisdictionArea = _jurisdictionArea;
 
+        emit BranchUpdate(
+            id,
+            _precinctAddress,
+            _jurisdictionArea
+        );
+
+    }
+
+    function _validateSignatures(
+        bytes32 _hash,
+        bytes[] memory _signatures,
+        address[] memory _signers,
+        uint _jurisdictionArea
+    ) public view {
+        bytes32 _messageHash = _hashTypedDataV4(_hash);
+        for (uint i = 0; i < _signatures.length; ++i) {
+            if(!(
+                SignatureChecker.isValidSignatureNow(_signers[i], _messageHash, _signatures[i]) && moderators[_signers[i]][_jurisdictionArea]
+            )) revert InvalidSignature();
+        }   
+    }
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32 domainSeparator) {
+        domainSeparator = _domainSeparatorV4();
     }
 
 }
--- back-end/contracts/test/Ledger.t.sol
+++ back-end/contracts/test/Ledger.t.sol
@@ -3,35 +3,67 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "./../src/Ledger.sol";
+import "./../src/Libraries/CreateBranch.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract OfficersTest is Test {
+
+    using CreateBranch for CreateBranch.CreateBranchVote;
 
     Ledger ledger;
     address ZER0_ADDRESS = address(0);
     bytes32 PRECINCT1 = keccak256(abi.encode("PRECINCT 1"));
-    address moderator = address(1); //Bob
+    uint256 _moderator1PrivateKey = 0xB0B;
+    address moderator1 = vm.addr(_moderator1PrivateKey); //0x0376AAc07Ad725E01357B1725B5ceC61aE10473c
+    uint256 _moderator2PrivateKey = 0xABB1;
+    address moderator2 = vm.addr(_moderator2PrivateKey); //0x4a79fB1C667Ff8AF3e5B50925747AA39D9f74262
     address captain1 = address(2); //E11
     address captain2 = address(3);
-    address detective1 = address(4); //A11CE
+    uint256 _alicePrivateKey = 0xA11CE;
+    address detective1 = vm.addr(_alicePrivateKey); //0xe05fcC23807536bEe418f142D19fa0d21BB0cfF7
 
     function setUp() public {
-        // ledger = new Officers(
-        //     moderator,
-        //     "Bob",
-        //     keccak256(abi.encode("BOB-1")),
-        //     PRECINCT1
-        // );
+        ledger = new Ledger(
+            moderator1,
+            "Bob",
+            keccak256(abi.encode("BOB-1")),
+            PRECINCT1
+        );
         // testOnboardCaptainByModerator();
 
     }
 
     function testNewBranch() public {
-        vm.startPrank(moderator);
+        bytes32 messageHash = CreateBranch.hash(CreateBranch.CreateBranchVote(
+            "5th Avenue",
+            88886,
+            PRECINCT1
+        ));
+
+        bytes[] memory signatures = new bytes[](2);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_moderator1PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator1Signature = abi.encodePacked(r, s, v);
+        
+        (v, r, s) = vm.sign(_moderator2PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator2Signature = abi.encodePacked(r, s, v);
+
+        signatures[0] = moderator1Signature;
+        signatures[1] = moderator2Signature;
+
+        address[] memory signers = new address[](2);
+
+        signers[0] = moderator1;
+        signers[1] = moderator2;
+        
+        vm.startPrank(moderator1);
 
         ledger.createBranch(
             "PRECINCT 1",
             "5th Avenue",
-            88886
+            88886,
+            signatures,
+            signers
         );
 
         (
@@ -48,44 +80,136 @@ contract OfficersTest is Test {
     }
 
     function testDuplicateBranch() public {
+        bytes32 messageHash = CreateBranch.hash(CreateBranch.CreateBranchVote(
+            "5th Avenue",
+            88886,
+            PRECINCT1
+        ));
+
+        bytes[] memory signatures = new bytes[](2);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_moderator1PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator1Signature = abi.encodePacked(r, s, v);
+        
+        (v, r, s) = vm.sign(_moderator2PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator2Signature = abi.encodePacked(r, s, v);
+
+        signatures[0] = moderator1Signature;
+        signatures[1] = moderator2Signature;
+
+        address[] memory signers = new address[](2);
+
+        signers[0] = moderator1;
+        signers[1] = moderator2;
+
         testNewBranch();
 
-        vm.startPrank(moderator);
+        vm.startPrank(moderator1);
 
         vm.expectRevert(BranchAlreadyExists.selector);
         ledger.createBranch(
             "PRECINCT 1",
             "5th Avenue",
-            88886
+            88886,
+            signatures,
+            signers
         );
 
         vm.stopPrank();
     }
 
     function testBranchIncorrectInput() public {
-        vm.startPrank(moderator);
 
-        vm.expectRevert(InvalidId.selector);
+        bytes32 messageHash = CreateBranch.hash(CreateBranch.CreateBranchVote(
+            "5th Avenue",
+            88886,
+            PRECINCT1
+        ));
+
+        bytes[] memory signatures = new bytes[](2);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_moderator1PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator1Signature = abi.encodePacked(r, s, v);
+        
+        (v, r, s) = vm.sign(_moderator2PrivateKey, _hashTypedDataV4(messageHash));
+        bytes memory moderator2Signature = abi.encodePacked(r, s, v);
+
+        signatures[0] = moderator1Signature;
+        signatures[1] = moderator2Signature;
+
+        address[] memory signers = new address[](2);
+
+        signers[0] = moderator1;
+        signers[1] = moderator2;
+
+        bytes[] memory signaturesHalf = new bytes[](1);
+        signaturesHalf[0] = moderator1Signature;
+
+        address[] memory signersHalf = new address[](1);
+        signersHalf[0] = moderator1;
+
+        vm.startPrank(moderator1);
+
+        vm.expectRevert(InvalidInput.selector);
         ledger.createBranch(
             "",
             "5th Avenue",
-            88886
+            88886,
+            signatures,
+            signers
         );
 
-        vm.expectRevert(InvalidId.selector);
+        vm.expectRevert(InvalidInput.selector);
         ledger.createBranch(
             "PRECINCT 1",
             "",
-            88886
+            88886,
+            signatures,
+            signers
         );
 
-        vm.expectRevert(InvalidId.selector);
+        vm.expectRevert(OnlyModerator.selector);
         ledger.createBranch(
             "PRECINCT 1",
             "5th Avenue",
-            0
+            0,
+            signatures,
+            signers
+        );
+
+        vm.expectRevert(LengthMismatch.selector);
+        ledger.createBranch(
+            "PRECINCT 1",
+            "5th Avenue",
+            88886,
+            signaturesHalf,
+            signers
+        );
+
+        vm.expectRevert(NotEnoughSignatures.selector);
+        ledger.createBranch(
+            "PRECINCT 1",
+            "5th Avenue",
+            88886,
+            signaturesHalf,
+            signersHalf
+        );
+
+        signers[0] = moderator2;
+
+        vm.expectRevert(InvalidSignature.selector);
+        ledger.createBranch(
+            "PRECINCT 1",
+            "5th Avenue",
+            88886,
+            signatures,
+            signers
         );
 
         vm.stopPrank();
+    }
+
+    function _hashTypedDataV4(bytes32 structHash) internal view virtual returns (bytes32) {
+        return ECDSA.toTypedDataHash(ledger.DOMAIN_SEPARATOR(), structHash);
     }
 }
