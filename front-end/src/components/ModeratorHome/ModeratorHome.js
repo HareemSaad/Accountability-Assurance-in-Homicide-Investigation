91a32eb21a680d09a8eb58c510df55fe336d7baf
Amaim Shaikh <amaimshaikhh@gmail.com>
2023-12-23 18:16:11 +0500

    update moderator home

--- front-end/src/components/ModeratorHome/ModeratorHome.js
+++ front-end/src/components/ModeratorHome/ModeratorHome.js
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+// import './ArchiveEmployees.css';
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from "react-router-dom";
-// import { useUserContext } from '../Context/userContext.tsx';
-
 
 export const ModeratorHome = () => {
-    // const { user, setUser } = useUserContext();
 
-    const [allRequests, setAllRequests] = useState([]);
+    const requestCategory = [{ "name": "Create Branch", "end-point": "create-branch" }, { "name": "Officer Onboard", "end-point": "officer-onboard" }, { "name": "Trustee Request", "end-point": "trustee-request" }, { "name": "Update Branch", "end-point": "update-branch" }, { "name": "Update Officer", "end-point": "update-officer" }];
+
     let navigate = useNavigate();
 
-    useEffect(() => {
-        const allRequests = [{ id: 13, status: "Open" }, { id: 27, status: "Open" }, { id: 36, status: "Open" }, { id: 45, status: "Open" }];
-        setAllRequests(allRequests);
-    }, []);
-
-    function print(cardId) {
-        navigate(`/case-detail/${cardId}`);
+    function print(cardName) {
+        // navigate(`/${cardEndpoint}`);
+        if (cardName === "Create Branch"){
+            navigate(`/view-create-branch`);
+        } else if(cardName === "Officer Onboard") {
+            navigate(`/view-officer-onboard`);
+        } else if(cardName === "Trustee Request") {
+            navigate(`/view-trustee-request`);
+        } else if(cardName === "Update Branch") {
+            navigate(`/view-update-branch`);
+        } else if(cardName === "Update Officer") {
+            navigate(`/view-update-officer`);
+        }
     }
 
     const goto = (e) => {
@@ -27,6 +33,7 @@ export const ModeratorHome = () => {
 
     return (
         <>
+
             <div className="d-flex justify-content-between">
                 <h1 className='m-4'>Requests</h1>
                 <div className="d-flex">
@@ -37,16 +44,19 @@ export const ModeratorHome = () => {
                 </div>
             </div>
 
-            <div className='card-container'>
-                {allRequests.map((card, index) => (
-                    <Card className='case-card'>
-                        <h2 className='mb-3 mt-3 pb-5'>Request# {card.id}</h2>
-                        <button className='card-btn' onClick={() => print(card.id, card.status)}>View</button>
+            {/* According to index of status category choosen from the dropdown employees list is shown */}
+            <div className='emp-card-container'>
+                {requestCategory.map((card, index) => (
+                    <Card key={index} style={{ width: '18rem' }} className='emp-case-card'>
+                        <Card.Body>
+                            <Card.Title>{card.name}</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Request</Card.Subtitle>
+                            {/* <button className='emp-card-btn' onClick={() => print(card['end-point'])}>View Requests</button> */}
+                            <button className='emp-card-btn' onClick={() => print(card.name)}>View Requests</button>
+                        </Card.Body>
                     </Card>
                 ))}
             </div>
-
-            
         </>
     )
 }
