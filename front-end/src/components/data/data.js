126e7db45c22f6e2282510bc5188a0513ce2f169
AmaimShaikh <amaimshaikhh@gmail.com>
2024-01-16 01:38:34 +0500

    updateType map added

--- front-end/src/components/data/data.js
+++ front-end/src/components/data/data.js
@@ -1,19 +1,24 @@
 export const employmentStatusMap = new Map();
-employmentStatusMap.set("0", "Inactive")
-employmentStatusMap.set("1", "Active")
-employmentStatusMap.set("2", "Retired")
-employmentStatusMap.set("3", "Fired")
+employmentStatusMap.set(0, "Inactive")
+employmentStatusMap.set(1, "Active")
+employmentStatusMap.set(2, "Retired")
+employmentStatusMap.set(3, "Fired")
 
 export const rankMap = new Map();
-rankMap.set("1", "Officer")
-rankMap.set("2", "Detective")
-rankMap.set("3", "Captain")
-rankMap.set("4", "Moderator")
+rankMap.set(1, "Officer")
+rankMap.set(2, "Detective")
+rankMap.set(3, "Captain")
+rankMap.set(4, "Moderator")
 
 export const stateCodeMap = new Map();
 stateCodeMap.set("8888", "8888")
 
 export const branchIdMap = new Map();
 branchIdMap.set("PRECINCT 1", "PRECINCT 1")
+
+export const updateTypeMap = new Map();
+updateTypeMap.set(0, "Address")
+updateTypeMap.set(1, "Name")
+updateTypeMap.set(2, "Badge")
 // 0x1f27a4c30ebdfe4d5a0bb20d2a6c8681791b7470b195ab0e6ff1eabf846f607d
 // 0x9ad84d0b712c25d6169be94f42e24baf7b28c0b245fa6b36d34de1bc1c7cfcd0
