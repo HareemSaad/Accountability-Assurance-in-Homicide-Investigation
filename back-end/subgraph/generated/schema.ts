b0c382593593f3a5b5b2b22379417eebfb6a10e4
HareemSaad <hareems20014@outlook.com>
2024-01-19 18:38:26 +0500

    ledger

--- back-end/subgraph/generated/schema.ts
+++ back-end/subgraph/generated/schema.ts
@@ -2577,6 +2577,19 @@ export class Officer extends Entity {
     this.set("id", Value.fromBytes(value));
   }
 
+  get name(): string {
+    let value = this.get("name");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toString();
+    }
+  }
+
+  set name(value: string) {
+    this.set("name", Value.fromString(value));
+  }
+
   get legalNumber(): Bytes {
     let value = this.get("legalNumber");
     if (!value || value.kind == ValueKind.NULL) {
