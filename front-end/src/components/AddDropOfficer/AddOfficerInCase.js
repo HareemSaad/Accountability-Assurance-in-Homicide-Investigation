7d5e4fe2cdb2fbc6e1b4d42715a4c1cd0e0a67f8
HareemSaad <hareems20014@outlook.com>
2024-01-20 20:09:41 +0500

    address dropdown

--- front-end/src/components/AddDropOfficer/AddOfficerInCase.js
+++ front-end/src/components/AddDropOfficer/AddOfficerInCase.js
@@ -1,27 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useParams } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { notify } from "./../utils/error-box/notify";
 import "react-toastify/dist/ReactToastify.css";
 import '../AddCase/AddCase.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 import CaseABI from "./../Cases.json";
-import { readContract, signMessage, waitForTransaction, writeContract } from '@wagmi/core'
+import { waitForTransaction, writeContract } from '@wagmi/core'
+import { client } from '../data/data';
+import { rankMap } from '../data/data';
 
 export const AddOfficerInCase = () => {
     const { caseId } = useParams();
-    let navigate = useNavigate();
+    // let navigate = useNavigate();
     const caseContractAddress = process.env.REACT_APP_CASE_CONTRACT;
 
     const [officerAddress, setOfficerAddress] = useState("");
+    const [officersInCase, setOfficersInCase] = useState([]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setOfficerAddress(value.toString());
-        // console.log("params :: ", name)
-        console.log("value :: ", value.toString())
-    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData() {
+        const query = `
+        {
+            officers(where: {branch: "${localStorage.getItem("branchid")}"}) {
+              id
+              name
+              rank
+            }
+        }
+        `;
+        const response = await client.query(query).toPromise();
+        const { data, fetching, error } = response;
+        console.log(data.officers);
+        setOfficersInCase(data.officers);
+    }
+
+    useEffect(() => {
+        console.log("officersInCase: ", officersInCase);
+    }, [officersInCase])
     
+    // Function to handle dropdown item selection
+    const handleDropdownSelect = async (value) => {
+        setOfficerAddress(value);
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         if(officerAddress === ''){
@@ -72,7 +98,26 @@ export const AddOfficerInCase = () => {
                         <label htmlFor="officerAddress" className="col-form-label"><b><em>Officer Address:</em></b></label>
                     </div>
                     <div className="col-9 input">
-                        <input type="text" name='address' id="officerAddress" placeholder='Enter officer Address Here' className="form-control" onChange={handleChange}></input>
+                        <Dropdown>
+                        <Dropdown.Toggle variant="light" id="rank" className="dropdown">
+                            {/* {selectedValue ? rankMap.get(selectedValue) : "Select Rank"} */}
+                            {officerAddress ? (officerAddress) : "Select Officer"}
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu className="dropdown">
+                        {officersInCase.length > 0 ? (
+                            officersInCase.map(element => (
+                                <Dropdown.Item key={element.id} name="rank" onClick={() => handleDropdownSelect(element.id)}>
+                                    {`${element.name} (${rankMap.get(element.rank)})`}
+                                </Dropdown.Item>
+                            ))
+                        ) : (
+                            <Dropdown.Item disabled>Loading officers...</Dropdown.Item>
+                        )}
+                        </Dropdown.Menu>
+                        </Dropdown>
+                        
+                        {/* <input type="text" name='address' id="officerAddress" placeholder='Enter officer Address Here' className="form-control" onChange={handleChange}></input> */}
                     </div>
                 </div>
 
